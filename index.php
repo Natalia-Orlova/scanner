7cdd2b3c23c194eba0b87f3ed4fdb138c272a0a7
natalia <natakarelskaya@yandex.ru>
2024-04-24 16:36:07 +0400

    index.html

--- index.php
+++ index.php
@@ -18,33 +18,33 @@
     const canvas = document.getElementById('qrcode');
     const context = canvas.getContext('2d');
 
-    // const img = new Image();
-    // img.onload = drawImageActualSize;
-    // img.src = 'img3.jpg';
+    const img = new Image();
+    img.onload = drawImageActualSize;
+    img.src = 'img3.jpg';
 
-    // function drawImageActualSize() {
-    //     canvas.width = this.naturalWidth;
-    //     canvas.height = this.naturalHeight;
-    //     context.drawImage(this, 0, 0);
-    //     // context.drawImage(this, 0, 0, this.width, this.height);
-    // }
+    function drawImageActualSize() {
+        canvas.width = this.naturalWidth;
+        canvas.height = this.naturalHeight;
+        context.drawImage(this, 0, 0);
+        // context.drawImage(this, 0, 0, this.width, this.height);
+    }
 
 
-    navigator.mediaDevices.getUserMedia({
-            video: true
-        })
-        .then(stream => {
-            video.srcObject = stream;
-            video.play();
+    // navigator.mediaDevices.getUserMedia({
+    //         video: true
+    //     })
+    //     .then(stream => {
+    //         video.srcObject = stream;
+    //         video.play();
 
-            // Draw the video frame onto the canvas every 100ms
-            setInterval(() => {
-                context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            }, 2000);
-        })
-        .catch(error => {
-            console.log('Error accessing camera:', error);
-        });
+    //         // Draw the video frame onto the canvas every 100ms
+    //         setInterval(() => {
+    //             context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    //         }, 2000);
+    //     })
+    //     .catch(error => {
+    //         console.log('Error accessing camera:', error);
+    //     });
 </script>
 
 </html>
